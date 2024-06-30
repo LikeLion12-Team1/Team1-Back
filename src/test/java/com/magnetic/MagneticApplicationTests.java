17e091bba506529ac5ef0ee6197a1d4d80019520
chamm99 <alslalsl3125@gmail.com>
2024-06-30 10:58:17 +0900

    :sparkles: feat: Project Initiate

---
+++ src/test/java/com/magnetic/MagneticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magnetic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagneticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
